9d65de9a9d2227057600f8fcdb6f54585a8b27a9
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-22 14:09:23 +0800

    feat: added delete feature in articles

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,33 +1,61 @@
 import Article from "../models/Article.js";
 
 export const getAllArticles = async (req, res) => {
-  try{
-
-  const articles = await Article.find({});
-  res.status(200).json({ message: "OK", articles});
-  }catch(err){
-    res.status(500).json({code: 500,message: "Internal Server Error"})
+  try {
+    const articles = await Article.find({});
+    res.status(200).json({ message: "OK", articles });
+  } catch (err) {
+    res.status(500).json({ code: 500, message: "Internal Server Error" });
   }
-
 };
 
 export const createArticle = async (req, res) => {
-    try{
-        const { title, photoUrl, description, sourceLink } = req.body;
-        const newArticle = await Article.create({
-            title,
-            photoUrl,
-            description,
-            sourceLink
-        })
+  try {
+    const { title, photoUrl, description, sourceLink } = req.body;
+    const newArticle = await Article.create({
+      title,
+      photoUrl,
+      description,
+      sourceLink,
+    });
 
-        res.status(201).json({
-            message: "Article added successfully.",
-            article: newArticle
-        })
-    }catch(err){
-        res.status(500).json({
-            message: "Internal Server Error"
-        })
+    res.status(201).json({
+      message: "Article added successfully.",
+      article: newArticle,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const deleteSingleArticle = async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    const article = Article.findByIdAndDelete(id);
+
+    if (!article) {
+      return res
+        .status(400)
+        .json({ code: 404, message: "Article nnot found." });
     }
-}
+    return res
+      .status(200)
+      .json({ code: 200, message: "Article deleted successfully." });
+  } catch (err) {
+    return res.json({ code: 500, message: "Internal Server Error." });
+  }
+};
+
+export const deleteAllArticle = async (req, res) => {
+  try {
+    const articles = Article.deleteMany({});
+    return res
+      .status(200)
+      .json({ code: 200, message: "Successfully deleted all articles." });
+  } catch (err) {
+    return res.status(500).json({code: 500, message: "Internal Server Error."})
+  }
+};
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { createArticle, getAllArticles } from '../controllers/articleController.js'
+import { createArticle, getAllArticles, deleteSingleArticle, deleteAllArticle } from '../controllers/articleController.js'
 import  protect, {adminOnly}  from '../middleware/verifyToken.js'
 
 const router = Router()
 
-router.get('/all', getAllArticles)
+router.get('/', getAllArticles)
 router.post('/createArticle',  protect, adminOnly , createArticle)
+router.delete('/deleteSingleArticle', protect, adminOnly, deleteSingleArticle)
+router.delete('/deleteAllArticle', protect, adminOnly, deleteAllArticle)
 
 export default router
