a1ed0ccaa069b530eaa2d1c121cb11f3ac653f54
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-24 11:12:20 +0800

    fix: cors 2

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import connectDB from './config/db.js'
-import router from './routes/index.js'
-const PORT = process.env.PORT || 5000
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import router from "./routes/index.js";
+const PORT = process.env.PORT || 5000;
 
+const app = express();
 
-const app = express()
+connectDB();
 
-connectDB()
-
-app.use(cors({ origin: "*", credentials: true }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // All routes go through /api
-app.use('/api', router)
+app.use("/api", router);
 
 app.listen(PORT, () => {
-    console.log(`Saferoute server is in ${PORT}`)
-})
+  console.log(`Saferoute server is in ${PORT}`);
+});
 
-process.on('uncaughtException', (err) => {
-  console.error('UNCAUGHT:', err.stack)
-})
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT:", err.stack);
+});
 
-process.on('unhandledRejection', (err) => {
-  console.error('UNHANDLED:', err.stack)
-})
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED:", err.stack);
+});
 
-export default app
+export default app;
