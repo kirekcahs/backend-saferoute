aeda01f0aa44c4bef1a033a354612480010d4246
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-24 13:09:33 +0800

    try: deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000" || process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
