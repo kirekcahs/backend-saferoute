4ee7ecf21d272f4361186ba4dd918784c4e3103a
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-24 11:08:25 +0800

    fix: cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 connectDB()
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cors({ origin: "*", credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
