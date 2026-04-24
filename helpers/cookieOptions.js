210e12452e5a848ec9a69b435d4b607633fefa6c
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-24 13:14:40 +0800

    fix: removed domain

--- helpers/cookieOptions.js
+++ helpers/cookieOptions.js
@@ -2,7 +2,6 @@ const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "none",
-  domain: ".railway.app",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
