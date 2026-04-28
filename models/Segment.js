4749e928ddd06e126802247d656a8d8e006b6724
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-28 16:49:28 +0800

    fix: points

--- models/Segment.js
+++ models/Segment.js
@@ -1,33 +1,36 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export const segmentSchema = new mongoose.Schema({
+export const segmentSchema = new mongoose.Schema(
+  {
     points: {
-        type: [[Number]], // Array of Arrays for Point A and Point B
-        required: true,
-        validate: {
-            validator: function(val) {
-                // 2 points (Start and End)
-                if (val.length !== 3) return false;
-                
-                // Both Point A and Point B must have exactly 2 numbers (lat, lon)
-                if (val[0].length !== 2 || val[1].length !== 2) return false;
-                
-                return true;
-            },
-            message: 'Points must contain exactly 2 coordinates: [[lat1, lon1], [lat2, lon2],[lat2, lon2]] '
-        }
+      type: [[Number]],
+      required: true,
+      validate: {
+        validator: function (val) {
+          // Must have 2 or 3 points
+          if (val.length < 2 || val.length > 3) return false;
+
+          // Each point must have exactly 2 numbers (lat, lon)
+          if (val[0].length !== 2 || val[1].length !== 2) return false;
+          if (val[2] && val[2].length !== 2) return false;
+
+          return true;
+        },
+        message:
+          "Points must contain 2 or 3 coordinates: [[lat1, lon1], [lat2, lon2]] or [[lat1, lon1], [lat2, lon2], [lat3, lon3]]",
+      },
     },
     coords: {
-        type: [[Number]],
-        required: true,
+      type: [[Number]],
+      required: true,
     },
-    floodReport:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'FloodReportAdmin',
-        default: null
+    floodReport: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "FloodReportAdmin",
+      default: null,
     },
-    
-}, {timestamps: true}
-)
+  },
+  { timestamps: true },
+);
 
-export default mongoose.model('Segment', segmentSchema)
+export default mongoose.model("Segment", segmentSchema);
