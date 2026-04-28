41891df4945a97fd4cd2e86530596caabfba62d8
kirekcahs <erikpoyaoan10@gmail.com>
2026-04-28 10:07:07 +0800

    fix: secure

--- helpers/cookieOptions.js
+++ helpers/cookieOptions.js
@@ -1,6 +1,6 @@
 const cookieOptions = {
   httpOnly: true,
-  secure: false, // false in dev, true in prod
+  secure: true, // false in dev, true in prod
   sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
