be0976673dad65bfe55a3ade6c63db4d13b9ff7d
kirekcahs <erikpoyaoan10@gmail.com>
2026-05-05 14:47:28 +0800

    fix: add get all endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createAdminAccount,
+  getAllAdmin,
   getAllUsers,
 } from "../controllers/userController.js";
 import protect, { adminOnly } from "../middleware/verifyToken.js";
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post("/createAdminAccount", protect, adminOnly, createAdminAccount);
 router.get("/all", protect, adminOnly, getAllUsers);
+router.get("/all/admin", protect, adminOnly, getAllAdmin)
 
 export default router;
